0fa06e07b1f75259d3af01380c283dc446aa29fc
matheushpr9 <113607124+matheushpr9@users.noreply.github.com>
2022-11-19 18:06:45 -0300

    Leitor de Arquivos

--- criando-bibliotecas/MinhaPrimeiraBibliotec/index.js
+++ criando-bibliotecas/MinhaPrimeiraBibliotec/index.js
@@ -1,4 +1,18 @@
-import chalk from 'chalk';
+import chalk from 'chalk'; // trocar cores e texto
+
+import fs from 'fs'; // biblioteca buit-in
+import { lstat } from 'fs/promises';
+
+function pega_arquivo(caminho_arquivo){
+
+    const encoding = 'UTF-8'
+
+    fs.readFile(caminho_arquivo, encoding, (_, conteudo)=>{
+        console.log(chalk.green(conteudo))
+    })
+}
+
+pega_arquivo('./arquivos/texto.md')
 
 console.log(chalk.blue('olá mundo'));
 
