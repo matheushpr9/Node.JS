de80a7417cc5376f5216533b58f5f5ec391da40c
matheushpr9 <113607124+matheushpr9@users.noreply.github.com>
2022-11-21 08:02:43 -0300

    Tratando novos erros e dando apossiblidade entranda, tanto como arquivo, quanto dirório

--- criando-bibliotecas/MinhaPrimeiraBibliotec/src/cli.js
+++ criando-bibliotecas/MinhaPrimeiraBibliotec/src/cli.js
@@ -1,10 +1,42 @@
+import chalk from "chalk";
+
+import fs from 'fs';
+
 import pega_arquivo from "./index.js";
 
 const caminho  = process.argv;
 
 async function processaTexto(caminho){
-    const resultado = await pega_arquivo(caminho[2]);
-    console.log(resultado)
+    
+
+    const path = caminho[2];
+
+    try{
+        fs.lstatSync(caminho);
+    }catch(erro){
+        if( erro.code === "ENOENT"){
+            console.log(chalk.red("Arquivo ou diretório não existe"));
+            return
+        }
+    }
+
+    if(fs.lstatSync(path).isFile()){
+        const resultado = await pega_arquivo(path);
+
+        console.log(
+            chalk.yellowBright('lista de Links :'),
+            chalk.black.bgGreen(path),
+            resultado);
+    } else if(fs.lstatSync(path).isDirectory()){
+        const arquivos = await fs.promises.readdir(path)
+        arquivos.forEach(async(nomeDeArquivo) => {
+            const lista = await pega_arquivo(`${path}/${nomeDeArquivo}`);
+            console.log("\n"+`${path}/${nomeDeArquivo}`+"\n")
+            console.log(lista)
+        })
+    }
+
+    
 }
 
 processaTexto(caminho)
