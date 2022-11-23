1eeb556250d488dbe9d38df18567a6d7f08714e6
matheushpr9 <113607124+matheushpr9@users.noreply.github.com>
2022-11-23 06:47:11 -0300

    Criando o escopo das rotas e inplementando o nodemon

--- criando-bibliotecas/API-REST-MongoDB/server.js
+++ criando-bibliotecas/API-REST-MongoDB/server.js
@@ -1,10 +1,15 @@
 const http = require("http")
-
 const porta = 3000;
+
+const rotas ={
+        '/' : 'Api em Node',
+        '/livros' : 'Entrei na página de çivros',
+        '/autores' : 'Listagem de autores'
+}
 
 const server  = http.createServer((req,res) =>{
     res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('API em Node');
+    res.end(rotas[req.url]);
     
 })
 
