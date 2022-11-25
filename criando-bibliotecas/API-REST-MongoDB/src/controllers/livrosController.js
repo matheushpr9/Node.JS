c8a8d41eb85f075bb15f927d3aebd1fc0820bd93
matheushpr9 <113607124+matheushpr9@users.noreply.github.com>
2022-11-25 09:17:14 -0300

    Relacionado dois documentos, e fazendo buscas especificas

--- criando-bibliotecas/API-REST-MongoDB/src/controllers/livrosController.js
+++ criando-bibliotecas/API-REST-MongoDB/src/controllers/livrosController.js
@@ -3,7 +3,8 @@ import livros from '../models/Livro.js'
 class LivroController{
 
     static listarLivros = (req, res) => {
-        livros.find((err,livros) =>{
+        livros.find().populate('autor')
+        .exec((err,livros) =>{
             res.status(200).json(livros)
         })
     }
@@ -11,7 +12,9 @@ class LivroController{
     static listarLivroPorId = (req,res) =>{
         const id = req.params.id;
 
-        livros.findById(id,(err,livro) =>{
+        livros.findById(id)
+        .populate('autor', 'nome')
+        .exec((err,livro) =>{
             if(err){
                 res.status(400).send({message:`${err.message} Id do livro não localizado`});
             }else{
@@ -51,6 +54,18 @@ class LivroController{
                 res.status(500).send({message:`${err.message} - Livro não encontrado`})
             }else{
                 res.status(200).send(`Item ${id} excluido com sucesso!`)
+            }
+        })
+    }
+
+    static listarLivroPorEditora = (req, res) =>{
+        const editora = req.query.editora;
+
+        livros.find({"editora" : editora}, {}, (err, livros)=>{
+            if(err){
+                res.status(400).send(`${err.message} - Editora não encontrada!`);
+            }else{
+                res.status(200).send(livros)
             }
         })
     }
